a1936c455c89693f1a89561f104ea6e563b67215
puhaiyang <puhaiyang@seanet.com.cn>
2024-07-25 15:36:20 +0800

    update bind address

--- send_recv_multicast/recv_multicast.py
+++ send_recv_multicast/recv_multicast.py
@@ -6,14 +6,13 @@ sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 # bind a multicast address
-multicast_group_ip = 'ff3e::2'
+multicast_group_ip = 'ffe8::2'
 port = 5007
 sock.bind(('::', port))
 
 # 告诉内核加入多播组
-# 第一个参数是IPv6多播地址
-# 第二个参数是接口索引（0表示系统默认接口）
 group = socket.inet_pton(socket.AF_INET6, multicast_group_ip)
+# 第二个参数是接口索引，此处修改为实际接口填写
 mreq = group + struct.pack('@I', 2)
 sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
 
@@ -23,8 +22,3 @@ print(f"绑定到组播地址 {multicast_group_ip}:{port}")
 while True:
     data, addr = sock.recvfrom(1024)
     print(f"Received message: {data} from {addr}")
-
-    # 回包逻辑
-    response = b'ACK: ' + data
-    sock.sendto(response, addr)
-    print(f"Sent response: {response} to {addr}")
